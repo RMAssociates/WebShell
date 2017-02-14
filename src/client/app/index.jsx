48d7d9a8bd714083317647c25dcfc14a977ca3ea
rmethani <rozina.methani@gmail.com>
2017-02-13 20:42:12 -0500

    Working Browser Sync

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+import React from 'react';
+import {render} from 'react-dom';
+// import Header from './components/header.jsx';
+
+class App extends React.Component {
+  render () {
+    return <p> Hello Ainafee 3</p>;
+  }
+}
+
+render(<App/>, document.getElementById('app'));
